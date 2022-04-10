76fae3868fb69544bcf7a5f1b5d900de0fe7d62a
Marina Gutkevich <gutmalina@mail.ru>
2022-04-10 16:45:50 +0300

    filter768-1440

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,33 +1,21 @@
 export class Filter{
   constructor( button){
-
     this._button = button;
-
   }
 
+//проверить условия фильтра, скрыть или показать карточку
   isGroupCard(){
     this._group = document.querySelector('.projects__group-cards');
     this._itemsArray = this._group.querySelectorAll('.projects__card');
     this._itemsArray.forEach((item) => {
       this._item = item;
-      this._itemId = item.querySelector('.projects__filter-group').textContent;
-
-      console.log(this._itemId)
-      console.log(this._button.id)
-      console.log(this._item)
-
-
-      if(this._button.id !== this._itemId){
-        this._item.classList.add('projects__card_hide')
-      }else{
+      this._itemId = this._item.querySelector('.projects__filter-group').textContent;
+      if(this._button.id === this._itemId || this._button.id === 'allProjects'){
         this._item.classList.remove('projects__card_hide')
+      }else{
+        this._item.classList.add('projects__card_hide')
       }
-
-    })
-
-
+    });
   }
-
- _toggleHiddenCard(){}
 
 }
